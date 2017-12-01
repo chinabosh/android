45f9456a78049bff03ab9ac47bd0b2c0df99b214
chinabosh <810916259@qq.com>
2017-12-01 17:28:37 +0800

    Update Singleton.java

--- Design/singleton/Singleton.java
+++ Design/singleton/Singleton.java
@@ -1,7 +1,7 @@
 public class Singleton {  
     private volatile static Singleton singleton;
     
-    private Singleton (){}  
+    private Singleton (){}  //构造方法私有，确保其他地方无法创建实例
     
     public static Singleton getSingleton() {  
     if (singleton == null) {  
