24f35bc925bfc9441e8ea1743847ef0e5fee699d
chianbosh <chinabosh@github.com>
2018-07-05 09:13:52 +0800

    project 单独放一个仓库

--- Demo/mylibrary/src/main/java/com/china/bosh/mylibrary/ui/activity/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package com.china.bosh.mylibrary.ui.activity;
-
-/**
- * @author lzq
- * @date 2018/7/4
- */
-
-public class BaseActivity {
-}
---
+++ material/annotation/readme.md
@@ -0,0 +1,3 @@
+这边博客感觉写的非常好，对于对注解一无所知的小白来说简直是福音。
+
+https://blog.csdn.net/briblue/article/details/73824058
