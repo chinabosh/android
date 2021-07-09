5e6a49ca08b3c052b3b161b97b1af20c823455fa
bosh <810916259@qq.com>
2021-07-09 16:21:06 +0800

    一些笔记

---
+++ git/git查看分支历史详情.md
@@ -0,0 +1,2 @@
+git log --graph --decorate --oneline --all
+//可以查看分支的历史,包括从哪里分支出来的
--- shell/设备截图提取.sh
+++ shell/设备截图提取.sh
@@ -4,3 +4,6 @@ adb shell screencap -p /sdcard/${time}.jpg
 adb pull /sdcard/${time}.jpg /Users/bosh/Downloads/screenshot
 #若手机内不保存截图，可把下面这句打开
 #adb shell rm /sdcard/${time}.jpg
+#-d 参数可以选择要截图的屏幕,可以用在具有双屏或者多屏的系统中
+#0一般为主屏,1一般为副屏
+adb shell screencap -d 0 /mnt/sdcard/screen0.jpg
